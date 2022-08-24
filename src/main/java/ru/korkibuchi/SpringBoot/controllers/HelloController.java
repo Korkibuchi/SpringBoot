28777b27c69ef139c3fc42e7a07e362dd7cbf32b
Korkibuchi <96173795+ KorkibKorkibuchi@users.noreply.github.com>
2022-08-24 20:05:22 +0300

    Initial commit

---
+++ src/main/java/ru/korkibuchi/SpringBoot/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package ru.korkibuchi.SpringBoot.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+
+    @GetMapping("/hello")
+    public String hello(){
+        
+        return "/hello";
+    }
+}
